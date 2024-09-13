de0e1635451b6243da55a8bb2911f5cfea6bb05b
joao <joao.ano03@gmail.com>
2024-09-13 09:18:05 -0300

    Ref: Task Serializers - Retirados os comentários didáticos

--- backend/tasks/serializers.py
+++ backend/tasks/serializers.py
@@ -1,22 +1,3 @@
-#Serializer
-"""
-- Responsável por converter dados complexos, como objetos do modelo Django em formatos JSON
-- Valida os dados recebidos para garantir que sejam salvos da forma correta no DB
-- Pode ser comparado com o Forms
-"""
-#Forms x Serializer
-"""
-Forms:
-    - Usado para lidar com dados em páginas web (HTML), validando entradas de usuários e gerando formulários
-    - Lida com HTML e POST request de formulário
-    - Retorna erro de validação em página HTML
-
-Serializer:
-    - Usado em API para converter dados entre JSON (ou outro formato) e objetos do Django, além de validar esses dados
-    - Lida com JSON (ou XML) em APIs
-    - Retorna erro em JSON com status de erro
-"""
-
 from rest_framework import serializers
 from .models import Item, TaskList
 
@@ -32,12 +13,3 @@ class TaskListSerializer(serializers.ModelSerializer):
         model = TaskList
         #Para incluir campos personalizados
         fields = ["name"] #Caso utilize mais campos -> ["name","name1","name2"]
-
-"""
-Quando utilizar campos personalizados:   
-    - Se o usuário não precisa preencher
-    - Se o usuário não precisa visualizar
-    - Se o campo é criado de forma automática
-    - Se o campo não é necessário em uma listagem (Otimização)
-    - Se o campo contém um dado sensível (Ex:senha)
-"""
